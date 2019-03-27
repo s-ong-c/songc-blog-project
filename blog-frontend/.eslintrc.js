63f7c481b5af9b1e76d53851e8e2c5943d414355
s-ong-c <somony9292@gmail.com>
2019-03-27 23:27:50 +0900

    paga

---
+++ blog-frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  "extends": [
+    "react-app",
+    "airbnb"
+  ],
+  "settings": {
+    "import/resolver": {
+      node: { paths: [path.resolve('./src')] }
+    },
+  },
+  "rules": {
+    "class-methods-use-this": 0,
+    "react/jsx-filename-extension": 0,
+    "jsx-a11y/href-no-hash": 0,
+    "no-unused-vars": 1,
+    "arrow-body-style": 0,
+    "import/prefer-default-export": 1,
+    "react/prop-types": 0,
+    "object-curly-spacing":0,
+    "react/self-closing-comp": 1,
+    "lines-between-class-members":0,
+    "react/jsx-indent":0,
+    "react/jsx-one-expression-per-line":0,
+    "react/prefer-stateless-function": 0,
+    "react/default-props-match-prop-types": 0
+  }
+}
--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,25 +1,26 @@
+// @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+type Props = {
+  something: string
+};
+
+type State ={
+  count: number
+}
+
+class App extends Component<Props, State> {
+  state ={
+    count: 0,
+  }
   render() {
+    const { count} = this.state;
+    const { something} = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {count}{something}
       </div>
     );
   }
