df7e0244d0870d46d8e5c0fa54842e8f32389ddc
s-ong-c <somony9292@gmail.com>
2019-05-11 19:26:30 +0900

    templates del

--- blog-frontend/src/containers/landing/LandingTemplateContainer.js
+++ blog-frontend/src/containers/landing/LandingTemplateContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LandingTemplate from '../../components/templates/LandingTemplate';
+import LandingTemplate from '../../components/landing/LandingTemplate';
 import withUser from '../../lib/hoc/withUser';
 const LandingTemplateContainer = (props) => {
     if (props.user) return null;
